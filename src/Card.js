697bc102bb1c4f5d7d353dd9c111137e91db4444
dreamywarrior <dreamywarrior1@gmail.com>
2022-02-12 12:29:17 +0530

    commit on 'master'

--- src/Card.js
+++ src/Card.js
@@ -1,10 +1,10 @@
 export default function Card(props){
     return(
         <div className = "Webpage--Card">
-            <img src = {props.imageUrl} className = "Webpage--place--image"/>
+            <img src = {props.imageUrl} className = "Webpage--place--image" alt = "places"/>
             <div className = "Webpage--Content">
                 <div className = "Webpage--Content--Level1">
-                    <img src = "../pictures/location_symbol.png" className = "Webpage--Location--icon"/>
+                    <img src = "../pictures/location_symbol.png" className = "Webpage--Location--icon" alt = "location_symbol"/>
                     <h4 className = "Webpage--Country">{props.location}</h4>
                     <a href = {props.googleMapsUrl} className = "Webpage--Maps">View on Google Maps</a>
                 </div>
